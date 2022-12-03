923f3cf65ea65bfe64c1c9f80f680e0018706ccc
q1 <zonggeu@gmail.com>
2022-12-03 18:18:57 +0800

    searchGoods

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import json
 import pymysql
 
 token = "a8b8ea51353e49c1ac23039e4586969d"
+
+
 # 根据类别获取商品列表
 def getGoodsListOfCategory(current=1, twoCategoryId=5):
-    url = 'https://bjsc.szbaoly.com/api/agent/pageGoods?current=' + str(current) + '&size=10&total=-1&twoCategoryId=' + str(twoCategoryId) + '&keyword='
+    url = 'https://bjsc.szbaoly.com/api/agent/pageGoods?current=' + str(
+        current) + '&size=10&total=-1&twoCategoryId=' + str(twoCategoryId) + '&keyword='
     headersData = {
         "Host": "bjsc.szbaoly.com",
         "appId": "wxeed6d656b89aeef3",
@@ -27,7 +30,7 @@ def getGoodsListOfCategory(current=1, twoCategoryId=5):
     response = requests.post(url, headers=headersData, verify=False)
     # 将Json格式字符串转字典
     content = json.loads(response.text)
-    print("HTTP::getGoodsListOfCategory::"+str(content))
+    print("HTTP::getGoodsListOfCategory::" + str(content))
 
 
 # 根据类别获取商品列表
@@ -54,20 +57,37 @@ def getCategoryList():
     response = requests.post(url, headers=headersData)
     # 将Json格式字符串转字典
     content = json.loads(response.text)
-    print("HTTP::getGoodsListOfCategory::"+str(content))
+    print("HTTP::getGoodsListOfCategory::" + str(content))
+
 
 def jsonOption():
     # use mock json to test load.
     content = json.loads('{"a":1,"data":[]}')
     # use "[" and "]" center input key to fetch value.
-    print("HTTP::jsonOption::"+str(content['a']))
+    print("HTTP::jsonOption::" + str(content['a']))
+
+
+def searchGoods(cursor, store_name):
+    # SQL 查询语句
+    sql = "SELECT * FROM `db_mini_shop`.`eb_store_product` WHERE store_name = '%s'" % (store_name)
+    try:
+        # 执行SQL语句
+        cursor.execute(sql)
+        # 获取所有记录列表
+        results = cursor.fetchall()
+        # 打印结果
+        print("Search goods result is %s" % \
+              (str(results)))
+    except:
+        print("Error: unable to fetch data")
+
 
 def optiomSql():
     # 打开数据库连接
     db = pymysql.connect(host='localhost',
                          user='root',
                          password='Budengyu1.',
-                         database='live_php')
+                         database='db_mini_shop')
     # 使用 cursor() 方法创建一个游标对象 cursor
     cursor = db.cursor()
     # 使用 execute()  方法执行 SQL 查询
@@ -75,8 +95,10 @@ def optiomSql():
     # 使用 fetchone() 方法获取单条数据.
     data = cursor.fetchone()
     print("Database version : %s " % data)
+    searchGoods(cursor, '展艺进口无盐黄油20粒动物煎牛排专用奶油面包饼干烘培家用小包装')
     # 关闭数据库连接
     db.close()
+
 
 if __name__ == '__main__':
     optiomSql()
