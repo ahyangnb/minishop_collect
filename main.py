8b815f26d785239dd40d0f537c0a7c3d7b90d2ed
q1 <zonggeu@gmail.com>
2022-12-03 19:32:34 +0800

    插入语句

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ def jsonOption():
 
 def searchGoods(cursor, store_name):
     # SQL 查询语句
-    sql = "SELECT * FROM `db_mini_shop`.`eb_store_product` WHERE store_name = '%s'" % (store_name)
+    sql = "SELECT * FROM `eb_store_product` WHERE store_name = '%s'" % (store_name)
     try:
         # 执行SQL语句
         cursor.execute(sql)
@@ -79,6 +79,25 @@ def searchGoods(cursor, store_name):
         print("Search goods result is %s" % (str(results)))
     except:
         print("Error: unable to fetch data")
+
+
+def innsertData(cursor, db):
+    # SQL 插入语句
+    sql = """INSERT INTO `eb_store_product` (`mer_id`, `image`, `recommend_image`, `slider_image`, `store_name`, `store_info`, `keyword`, `bar_code`, `cate_id`, `price`, `vip_price`, `ot_price`, `postage`, `unit_name`, `sort`, `sales`, `stock`, `is_show`, `is_hot`, `is_benefit`, `is_best`, `is_new`, `is_virtual`, `virtual_type`, `add_time`, `is_postage`, `is_del`, `mer_use`, `give_integral`, `cost`, `is_seckill`, `is_bargain`, `is_good`, `is_sub`, `is_vip`, `ficti`, `browse`, `code_path`, `soure_link`, `video_link`, `temp_id`, `spec_type`, `activity`, `spu`, `label_id`, `command_word`, `recommend_list`, `vip_product`, `presale`, `presale_start_time`, `presale_end_time`, `presale_day`, `logistics`, `freight`, `custom_form`) VALUES
+(0, 'https://data44.wuht.net//uploads/attach/2022/01/15/8a2d668e1b8fde3ed9422c242eedbb32.jpg', '',
+ '[\"https:\\/\\/data44.wuht.net\\/\\/uploads\\/attach\\/2022\\/01\\/15\\/8a2d668e1b8fde3ed9422c242eedbb32.jpg\",\"https:\\/\\/data44.wuht.net\\/\\/uploads\\/attach\\/2022\\/01\\/15\\/b4dc68ca453c74fda4ffcf385b0d4414.jpg\",\"https:\\/\\/data44.wuht.net\\/\\/uploads\\/attach\\/2022\\/01\\/15\\/76559ec8017c8ac68ea5ecac8145b56c.jpg\",\"https:\\/\\/data44.wuht.net\\/\\/uploads\\/attach\\/2022\\/01\\/15\\/409219640e8cd3b347c306b566785f2d.jpg\",\"https:\\/\\/data44.wuht.net\\/\\/uploads\\/attach\\/2022\\/01\\/15\\/742ec1ba074a8cc3a95839bb230c4244.jpg\",\"https:\\/\\/data44.wuht.net\\/\\/uploads\\/attach\\/2022\\/01\\/15\\/680b6fb7bbbcba59bb8fd015b63413c0.jpg\",\"https:\\/\\/data44.wuht.net\\/\\/uploads\\/attach\\/2022\\/01\\/15\\/1294869126a770d8519686eec84c9734.jpg\",\"https:\\/\\/data44.wuht.net\\/\\/uploads\\/attach\\/2022\\/01\\/15\\/c3936b8d5ff3195edda9c3427c18a7d3.jpg\"]'
+ , '111优家UPLUS ins风简约无痕发夹金属一字夹4个装（金色+银色 刘海边夹前额一字发卡碎发夹卡子夹子分发夹）', '优家UPLUS ins风简约无痕发夹金属一字夹4个装（金色+银色 刘海边夹前额一字发卡碎发夹卡子夹子分发夹）', '', 
+ '', '4', '15.90', '0.00', '0.00', '0.00', '件', 0, 0, 400, 1, 1, 1, 1, 1, 0, 0, 1642241487, 0, 0, 0, '0.00', '15.90', 0, 0, 1, 0, 0, 0, 0, '', '', '', 1, 1, '0,1,2,3',
+  '1025349510439', '', '', '', 0, 0, 0, 0, 0, '1,2', 3, '[]');"""
+    try:
+        # 执行sql语句
+        cursor.execute(sql)
+        # 提交到数据库执行
+        db.commit()
+        print("data innser success.")
+    except:
+        # 如果发生错误则回滚
+        db.rollback()
 
 
 def optiomSql():
@@ -94,7 +113,8 @@ def optiomSql():
     # 使用 fetchone() 方法获取单条数据.
     data = cursor.fetchone()
     print("Database version : %s " % data)
-    searchGoods(cursor, '展艺进口无盐黄油20粒动物煎牛排专用奶油面包饼干烘培家用小包装')
+    searchGoods(cursor, 's王府井')
+    # innsertData(cursor, db)
     # 关闭数据库连接
     db.close()
 
