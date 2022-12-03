a00b2377dd8ef799933e99866c34685d2d42ddb4
q1 <zonggeu@gmail.com>
2022-12-03 21:40:05 +0800

    SEARCH::searchGoods::

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def searchGoods(cursor, store_name):
         # 获取所有记录列表
         results = cursor.fetchall()
         # 打印结果
-        print("Search goods result is %s" % (str(results)))
+        print("SEARCH::searchGoods:: Search goods result is %s" % (str(results)))
         if str(results) == '()':
             return str(results)
         return None
