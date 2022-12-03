40e0b138512491543f6c20b9aa9a992fc6329746
q1 <zonggeu@gmail.com>
2022-12-03 16:42:11 +0800

    操作数据库

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests  # 导入requests包
 import json
+import pymysql
 
 token = "a8b8ea51353e49c1ac23039e4586969d"
 # 根据类别获取商品列表
@@ -61,5 +62,21 @@ def jsonOption():
     # use "[" and "]" center input key to fetch value.
     print("HTTP::jsonOption::"+str(content['a']))
 
+def optiomSql():
+    # 打开数据库连接
+    db = pymysql.connect(host='localhost',
+                         user='root',
+                         password='Budengyu1.',
+                         database='live_php')
+    # 使用 cursor() 方法创建一个游标对象 cursor
+    cursor = db.cursor()
+    # 使用 execute()  方法执行 SQL 查询
+    cursor.execute("SELECT VERSION()")
+    # 使用 fetchone() 方法获取单条数据.
+    data = cursor.fetchone()
+    print("Database version : %s " % data)
+    # 关闭数据库连接
+    db.close()
+
 if __name__ == '__main__':
-    jsonOption()
+    optiomSql()
